7f88445c67af2ad6669e9f0896e4b27931c4ac8b
Michael Hofmann <mho@next-munich.com>
2021-08-07 10:25:40 +0200

    Inital commit

---
+++ app/src/test/java/com/example/mystocksgo/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package com.example.mystocksgo
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val expected = 4
+        val actual = 2 + 2
+        assertEquals(expected, actual)
+    }
+}
