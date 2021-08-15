646128bc28980d0e5abb3664d61f1e3ec9644a30
Michael Hofmann <mho@next-munich.com>
2021-08-16 00:38:05 +0200

    Add nav graph

--- app/src/main/java/com/example/mystocksgo/MainActivity.kt
+++ app/src/main/java/com/example/mystocksgo/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.mystocksgo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-        recyclerView.hasFixedSize()
-        recyclerView.adapter = InstrumentAdapter()
     }
 }
