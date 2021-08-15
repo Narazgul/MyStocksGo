cb5588f58205e6c82dddabd3e9c00dd1522a4045
Michael Hofmann <mho@next-munich.com>
2021-08-16 00:56:38 +0200

    Remove Retrofit logging Interceptor

--- app/src/main/java/com/example/mystocksgo/di/AppModule.kt
+++ app/src/main/java/com/example/mystocksgo/di/AppModule.kt
@@ -6,8 +6,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,15 +14,11 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    private val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit =
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
-            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
