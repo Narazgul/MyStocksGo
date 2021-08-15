43c177abb5fcf52da1f78965c3f78d5fd67d226c
Michael Hofmann <mho@next-munich.com>
2021-08-16 00:36:35 +0200

    Add Instrument for domain level

---
+++ app/src/main/java/com/example/mystocksgo/domain/Instrument.kt
@@ -0,0 +1,9 @@
+package com.example.mystocksgo.domain
+
+data class Instrument(
+    val id: Int,
+    val name: String,
+    val exchangeName: String,
+    val quoteValue: String,
+    val unitName: String
+)
